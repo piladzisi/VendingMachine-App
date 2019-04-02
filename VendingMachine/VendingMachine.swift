66b8fed4b054a7b8e4ef15b6b1242bf289682b6c
Anna <piladzisi@gmail.com>
2019-04-02 17:18:21 +0300

    Initial Commit

---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,62 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by anna.sibirtseva on 02/04/2019.
+//  Copyright © 2019 Treehouse Island, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum VendingSelection{
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+protocol VendingItem{
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
