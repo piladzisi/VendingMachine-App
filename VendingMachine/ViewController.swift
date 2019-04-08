3ed8f0ad01ccb8d493f15c333df59de530fa4a63
Anna <piladzisi@gmail.com>
2019-04-08 17:58:05 +0300

    full of bugs. Why funds go to 0 after error alert? why buttons only work on second try?

--- VendingMachine/ViewController.swift
+++ VendingMachine/ViewController.swift
@@ -44,7 +44,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Setup
